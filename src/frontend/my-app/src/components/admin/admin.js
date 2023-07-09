6480867098cf96ec312aaebb171855c0986b95cc
winandtu <jyfy121@gmail.com>
2023-07-08 20:41:18 -0500

    actualización librerias

--- src/frontend/my-app/src/components/admin/admin.js
+++ src/frontend/my-app/src/components/admin/admin.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './admin.css'; // Importar el archivo CSS
 
-
 function Admin() {
   const [operarios, setOperarios] = useState([]);
   const [puntosRecolectados, setPuntosRecolectados] = useState([]);
@@ -15,7 +14,6 @@ function Admin() {
           const data = await response.json();
           const operariosFiltrados = data.filter((user) => user.role === 'operario');
           setOperarios(operariosFiltrados);
-          localStorage.setItem('operarios', JSON.stringify(operariosFiltrados)); // Almacenar en el almacenamiento local
         } else {
           console.error('Error al obtener la información de los operarios');
         }
@@ -24,12 +22,7 @@ function Admin() {
       }
     };
 
-    const storedOperarios = localStorage.getItem('operarios');
-    if (storedOperarios) {
-      setOperarios(JSON.parse(storedOperarios));
-    } else {
-      fetchOperarios();
-    }
+    fetchOperarios();
   }, []);
 
   // Obtener los puntos recolectados por los operarios desde el backend
@@ -40,7 +33,6 @@ function Admin() {
         if (response.ok) {
           const data = await response.json();
           setPuntosRecolectados(data);
-          localStorage.setItem('puntosRecolectados', JSON.stringify(data)); // Almacenar en el almacenamiento local
         } else {
           console.error('Error al obtener los puntos recolectados');
         }
@@ -49,87 +41,86 @@ function Admin() {
       }
     };
 
-    const storedPuntosRecolectados = localStorage.getItem('puntosRecolectados');
-    if (storedPuntosRecolectados) {
-      setPuntosRecolectados(JSON.parse(storedPuntosRecolectados));
-    } else {
-      fetchPuntosRecolectados();
-    }
+    fetchPuntosRecolectados();
   }, []);
 
   return (
     <div>
-      <h1>INFORMACIÓN</h1>
-      <div className="panel-container">
-        <div className="panel-left">
-          <div className="container">
-            <h1>Puntos Recolectados</h1>
-            <div className="table-container">
-              <table>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Nombre</th>
-                    <th>Operario</th>
-                    <th>Fecha</th>
-                  </tr>
-                </thead>
-                <tbody>
+      {operarios.length > 0 ? (
+        <div>
+          <h1>INFORMACIÓN</h1>
+          <div className="panel-container">
+            <div className="panel-left">
+              <div className="container">
+                <h1>Puntos Recolectados</h1>
+                <div className="table-container">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Operario</th>
+                        <th>Fecha</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {puntosRecolectados.map((punto, index) => (
+                        <tr key={punto.id}>
+                          <td>{index + 1}</td>
+                          <td>{punto.name}</td>
+                          <td>{punto.userId}</td>
+                          <td>{punto.create_date}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+
+              <div className="container">
+                <h1>Comentarios</h1>
+                <div className="map-box">
                   {puntosRecolectados.map((punto, index) => (
-                    <tr key={punto.id}>
-                      <td>{index + 1}</td>
-                      <td>{punto.name}</td>
-                      <td>{punto.userId}</td>
-                      <td>{punto.create_date}</td>
-                    </tr>
+                    <div key={punto.id}>
+                      <p>{punto.name}</p>
+                      <p>{punto.comments}</p>
+                    </div>
                   ))}
-                </tbody>
-              </table>
+                </div>
+              </div>
             </div>
-
-          </div>
-
-          <div className="container">
-            <h1>Comentarios &nbsp;&nbsp;&nbsp;</h1>
-            <div className='map-box'>
-              {puntosRecolectados.map((punto, index) => (
-                <tr key={punto.id}>
-
-                  <td>{punto.name}&nbsp;&nbsp;&nbsp;</td>
-                  <td>{punto.comments}</td>
-                </tr>
-              ))}
+            <div className="panel-right">
+              <div className="container">
+                <h1>Información de Operarios</h1>
+                <div className="table-container">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>username</th>
+                        <th>ID</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {operarios.map((operario, index) => (
+                        <tr key={operario.id}>
+                          <td>{index + 1}</td>
+                          <td>{operario.name}</td>
+                          <td>{operario.username}</td>
+                          <td>{operario.id}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="panel-right">
-          <div className="container">
-            <h1>Información de Operarios</h1>
-            <div className="table-container">
-              <table>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Nombre</th>
-                    <th>username</th>
-                    <th>ID</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {operarios.map((operario, index) => (
-                    <tr key={operario.id}>
-                      <td>{index + 1}</td>
-                      <td>&nbsp;&nbsp;&nbsp;{operario.name}&nbsp;&nbsp;&nbsp;</td>
-                      <td>{operario.username}&nbsp;&nbsp;&nbsp;</td>
-                      <td>{operario.id}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
+      ) : ( 
+        <p>Cargando...</p>
+      )}
     </div>
   );
 }
